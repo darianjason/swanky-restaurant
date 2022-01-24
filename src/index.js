b08cb356f462b9b0d85d215a4aedc28977380ad0
Darian Jason <darianjason@gmail.com>
2022-01-24 22:01:05 +0700

    Update index.js

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './stylesheets/reset.css';
+import './stylesheets/main.css';
+
+import loadPage from './modules/master';
+import loadHomeContent from './modules/home';
+
+loadPage();
+loadHomeContent();
